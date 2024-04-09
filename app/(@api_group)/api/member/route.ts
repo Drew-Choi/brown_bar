1afd72c1502f941eb73b0ffdfdf7d96b40cab37d
Drew <drc920630@gmail.com>
2024-04-09 16:31:01 +0900

    사용자권한변경 픽스 static을 dynamic으로

--- app/(@api_group)/api/member/route.ts
+++ app/(@api_group)/api/member/route.ts
@@ -2,6 +2,8 @@ import connectDB from '@/app/(@api_group)/api/_lib/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import Member from '@/app/(@api_group)/api/_models/Member';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     await connectDB();
